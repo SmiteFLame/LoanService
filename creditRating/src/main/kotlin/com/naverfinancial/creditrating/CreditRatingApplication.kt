ac11a4ecd377e3e9a7bfb09161ca3fc42c978249
SmiteFLame <lukigreen@naver.com>
2021-08-18 17:00:10 +0900

    ADD: 신용 평가 모듈

---
+++ creditRating/src/main/kotlin/com/naverfinancial/creditrating/CreditRatingApplication.kt
@@ -0,0 +1,11 @@
+package com.naverfinancial.creditrating
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditRatingApplication
+
+fun main(args: Array<String>) {
+    runApplication<CreditRatingApplication>(*args)
+}
---
+++ creditRating/src/test/kotlin/com/naverfinancial/creditrating/CreditRatingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.naverfinancial.creditrating
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditRatingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
