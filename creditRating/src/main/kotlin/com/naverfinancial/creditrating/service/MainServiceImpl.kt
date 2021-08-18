37f33b008b77af388fa81be2efb43d1d46bb3ee1
SmiteFLame <lukigreen@naver.com>
2021-08-18 18:48:20 +0900

    FEAT: 데이터 검색 후 검색 기록, 등급 기록 저장 기능

--- creditRating/src/main/kotlin/com/naverfinancial/creditrating/service/MainServiceImpl.kt
+++ creditRating/src/main/kotlin/com/naverfinancial/creditrating/service/MainServiceImpl.kt
@@ -1,5 +1,9 @@
 package com.naverfinancial.creditrating.service
 
+import com.naverfinancial.creditrating.dto.CreditRatingSearchHistory
+import com.naverfinancial.creditrating.dto.CreditRatingSearchResult
+import com.naverfinancial.creditrating.repository.CreditRatingSearchHistoryRepository
+import com.naverfinancial.creditrating.repository.CreditRatingSearchResultRepository
 import com.naverfinancial.creditrating.repository.UserRespository
 import com.naverfinancial.creditrating.utils.JsonFormData
 import com.naverfinancial.creditrating.wrapper.CreditResult
@@ -10,35 +14,64 @@ import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
+import java.sql.Timestamp
 
 @Service
 class MainServiceImpl : MainService {
 
     @Autowired
     lateinit var userRespository: UserRespository
+    @Autowired
+    lateinit var creditRatingSearchHistoryRepository: CreditRatingSearchHistoryRepository
 
-    override fun selectGrade(NDI : String) : CreditResult{
+    @Autowired
+    lateinit var creditRatingSearchResultRepository: CreditRatingSearchResultRepository
+
+
+
+    override fun selectGrade(NDI: String): CreditResult {
         var user = userRespository.findUserByNDI(NDI)
 
-        if(user == null){
+        if (user == null) {
             // 에러 처리
         }
 
-        val values = mapOf("age" to user.age.toString(), "salary" to user.salary.toString())
+        // 이미 검색된 결과가 존재하면 가져오기
+        //if(creditRatingSearchResultRepository.findCreditRatingSearchResultByNDI(user.getNDI()) != null){
+
+        //}
+
+        // CB 모듈에서 가져오는 기능은 다른 utils으로 옮기기
+        val values = mapOf("age" to user.getAge().toString(), "salary" to user.getSalary().toString())
         val client = HttpClient.newBuilder().build();
         val request = HttpRequest.newBuilder()
-                .uri(URI.create("http://localhost:8888/api/cb/grade"))
-                .POST(JsonFormData.formData(values))
-                .header("Content-Type", "application/x-www-form-urlencoded")
-                .build()
+            .uri(URI.create("http://localhost:8888/api/cb/grade"))
+            .POST(JsonFormData.formData(values))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .build()
         val response = client.send(request, HttpResponse.BodyHandlers.ofString());
         val grade = JSONObject(response.body()).getInt("grade")
         val isPermit = evaluateLoanAvailability(grade)
 
         // CreaditRatingSearchHistory 기록하기
+        var newCreditRatingSearchHistory =
+            CreditRatingSearchHistory(
+                history_id = -1, // AUTO_INCREASED
+                NDI = user.getNDI(),
+                grade = grade,
+                created_date = Timestamp(System.currentTimeMillis())
+            )
+        var resultOfCreditRatingSearchHistory = creditRatingSearchHistoryRepository.save(newCreditRatingSearchHistory)
 
+        println(resultOfCreditRatingSearchHistory.getHistoryId())
         // CreaditRatingSearchResult 기록하기
-
+        var newCreditRatingSearchResult =
+            CreditRatingSearchResult(
+                NDI = user.getNDI(),
+                grade = grade,
+                history_id = resultOfCreditRatingSearchHistory.getHistoryId()
+            )
+        creditRatingSearchResultRepository.save(newCreditRatingSearchResult)
 
         return CreditResult(grade, isPermit)
     }
