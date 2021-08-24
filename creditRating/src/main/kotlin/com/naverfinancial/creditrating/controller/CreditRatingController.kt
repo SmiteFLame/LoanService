209a219922f0c46cc01057f63bc6c464e91d6791
SmiteFLame <lukigreen@naver.com>
2021-08-24 17:33:46 +0900

    FEAT: Error Handler

--- creditRating/src/main/kotlin/com/naverfinancial/creditrating/controller/CreditRatingController.kt
+++ creditRating/src/main/kotlin/com/naverfinancial/creditrating/controller/CreditRatingController.kt
@@ -3,9 +3,11 @@ package com.naverfinancial.creditrating.controller
 import com.naverfinancial.creditrating.entity.creditRatingSearch.dto.CreditRatingSearchResult
 import com.naverfinancial.creditrating.enumClass.ExceptionEnum
 import com.naverfinancial.creditrating.service.CreditRatingService
+import com.naverfinancial.creditrating.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -13,9 +15,11 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/credits")
 class CreditRatingController {
 
+    @Autowired
+    lateinit var creditRatingService: CreditRatingService
 
     @Autowired
-    lateinit var mService: CreditRatingService
+    lateinit var userService: UserService
 
     /**
      * NDI을 입력받아서 유저 정보를 가져온 후 신용등급 및 대출 가능 여부 파악한다
@@ -26,19 +30,31 @@ class CreditRatingController {
      * GATEWAY_TIMEOUT - 10초 이내로 데이터 요청을 신용등급을 못 가져온 경우
      */
     @ExceptionHandler
-    fun exceptionHandler(e: Exception): ResponseEntity<String> {
-        when(e.message){
+    fun exceptionHandler(error : Exception): ResponseEntity<String> {
+        // 정의할 수 있는 에러 처리
+        when(error.message){
             ExceptionEnum.NOT_FOUND_NDI.toString() -> return ResponseEntity<String>("NDI가 요청되지 않음", HttpStatus.BAD_REQUEST)
             ExceptionEnum.NOT_FOUND_USER.toString() -> return ResponseEntity<String>("USER가 존재하지 않음", HttpStatus.BAD_REQUEST)
+        }
+
+        // 정의할 수 없는 에러 처리
+        when(error){
+            is HttpMessageNotReadableException -> return ResponseEntity<String>("JSON 데이터가 요청되지 않음", HttpStatus.BAD_REQUEST)
         }
         return ResponseEntity<String>("서버 에러", HttpStatus.INTERNAL_SERVER_ERROR)
     }
 
     @PostMapping
     fun selectGrade(@RequestBody map: Map<String, String>): ResponseEntity<CreditRatingSearchResult> {
+        println("A")
         if (!map.containsKey("ndi")) {
             throw Exception(ExceptionEnum.NOT_FOUND_NDI.toString())
         }
-        return ResponseEntity<CreditRatingSearchResult>(mService.selectGrade(map.getValue("ndi")), HttpStatus.OK)
+
+        var user = userService.selectUserByNDI(map.getValue("ndi"))
+        if(user == null){
+            throw Exception(ExceptionEnum.NOT_FOUND_USER.toString())
+        }
+        return ResponseEntity<CreditRatingSearchResult>(creditRatingService.selectGrade(user), HttpStatus.OK)
     }
 }
