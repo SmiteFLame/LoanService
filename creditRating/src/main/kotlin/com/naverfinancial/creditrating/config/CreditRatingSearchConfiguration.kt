79e6b18195a1f3457ff21176e5469a1e1ab392ab
SmiteFLame <lukigreen@naver.com>
2021-08-27 18:33:57 +0900

    FIX: 필요없는 autowired 삭제

--- creditRating/src/main/kotlin/com/naverfinancial/creditrating/config/CreditRatingSearchConfiguration.kt
+++ creditRating/src/main/kotlin/com/naverfinancial/creditrating/config/CreditRatingSearchConfiguration.kt
@@ -1,7 +1,5 @@
 package com.naverfinancial.creditrating.config
 
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.jdbc.DataSourceBuilder
 import org.springframework.context.annotation.Bean
@@ -11,7 +9,6 @@ import org.springframework.orm.jpa.JpaTransactionManager
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter
 import org.springframework.transaction.PlatformTransactionManager
-import javax.persistence.EntityManagerFactory
 import javax.sql.DataSource
 
 @Configuration
