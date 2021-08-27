2af34b76f3a494a61c2c8cfc624e51528a707461
SmiteFLame <lukigreen@naver.com>
2021-08-27 16:07:45 +0900

    FIX: Config 파일 위치 이동

--- creditRating/src/main/kotlin/com/naverfinancial/creditrating/config/entityConfig/CreditRatingSearchConfiguration.kt
+++ creditRating/src/main/kotlin/com/naverfinancial/creditrating/config/entityConfig/CreditRatingSearchConfiguration.kt
@@ -1,5 +1,6 @@
 package com.naverfinancial.creditrating.entity.creditRatingSearch.config
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.jdbc.DataSourceBuilder
@@ -7,8 +8,10 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.orm.jpa.JpaTransactionManager
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter
+import javax.persistence.EntityManagerFactory
 import javax.sql.DataSource
 
 @Configuration
@@ -32,3 +35,15 @@ class CreditRatingSearchConfiguration {
             jpaVendorAdapter = HibernateJpaVendorAdapter()
         }
 }
+
+
+class CreditRatingSearchJpaTransactionManager : JpaTransactionManager(){
+    @Qualifier("creditRatingSearchLocalContainerEntityManagerFactoryBean")
+    @Autowired
+    lateinit var creditRatingSearchEntityManager: LocalContainerEntityManagerFactoryBean
+
+    override fun getEntityManagerFactory(): EntityManagerFactory? {
+        return creditRatingSearchEntityManager.`object`
+    }
+}
+
--- creditRating/src/main/kotlin/com/naverfinancial/creditrating/config/entityConfig/UserConfiguration.kt
+++ creditRating/src/main/kotlin/com/naverfinancial/creditrating/config/entityConfig/UserConfiguration.kt
@@ -1,13 +1,16 @@
 package com.naverfinancial.creditrating.entity.user.config
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.jdbc.DataSourceBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.orm.jpa.JpaTransactionManager
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter
+import javax.persistence.EntityManagerFactory
 import javax.sql.DataSource
 
 @Configuration
@@ -30,3 +33,14 @@ class UserConfiguration {
             jpaVendorAdapter = HibernateJpaVendorAdapter()
         }
 }
+
+class UserJpaTransactionManager : JpaTransactionManager() {
+    @Qualifier("userLocalContainerEntityManagerFactoryBean")
+    @Autowired
+    lateinit var userEntityManager: LocalContainerEntityManagerFactoryBean
+
+    override fun getEntityManagerFactory(): EntityManagerFactory? {
+        return userEntityManager.`object`
+    }
+}
+
