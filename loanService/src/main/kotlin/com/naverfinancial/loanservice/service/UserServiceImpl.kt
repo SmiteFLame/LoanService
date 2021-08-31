634937b607e9e5f88da555f267be3dcd84675fd5
SmiteFLame <lukigreen@naver.com>
2021-08-31 15:05:34 +0900

    FIX: 예외 처리 방법 수정

--- loanService/src/main/kotlin/com/naverfinancial/loanservice/service/UserServiceImpl.kt
+++ loanService/src/main/kotlin/com/naverfinancial/loanservice/service/UserServiceImpl.kt
@@ -11,6 +11,7 @@ import org.json.JSONObject
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import java.net.ConnectException
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
@@ -46,29 +47,30 @@ class UserServiceImpl : UserService {
     }
 
     override fun insertUser(user: User): User {
-        val uuid = UUID.randomUUID().toString()
-        user.ndi = uuid
-
+        user.ndi = UUID.randomUUID().toString()
         return userRepository.save(user)
     }
 
-
     override fun searchGrade(ndi: String): CreditRatingSearchResult {
-        val values = mapOf("ndi" to ndi)
-        val client = HttpClient.newBuilder().build()
-        val request = HttpRequest.newBuilder()
-            .uri(URI.create("http://localhost:8081/credits"))
-            .POST(JsonFormData.formData(values))
-            .header("Content-Type", "application/json")
-            .build()
-        val response = client.send(request, HttpResponse.BodyHandlers.ofString())
-        if (response.statusCode() == 200) {
-            return CreditRatingSearchResult(
-                JSONObject(response.body()).getInt("grade"),
-                JSONObject(response.body()).getBoolean("isPermit")
-            )
-        } else {
-            throw UserException.CreditRatingException(response.body().toString(), HttpStatus.valueOf(response.statusCode()))
+        try{
+            val values = mapOf("ndi" to ndi)
+            val client = HttpClient.newBuilder().build()
+            val request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8081/credits"))
+                .POST(JsonFormData.formData(values))
+                .header("Content-Type", "application/json")
+                .build()
+            val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+            if (response.statusCode() == 200) {
+                return CreditRatingSearchResult(
+                    JSONObject(response.body()).getInt("grade"),
+                    JSONObject(response.body()).getBoolean("isPermit")
+                )
+            } else {
+                throw UserException.CreditRatingException(response.body().toString(), HttpStatus.valueOf(response.statusCode()))
+            }
+        }catch (e : ConnectException){
+            throw UserException.FailConnectCreditRatingServerException()
         }
     }
 }
