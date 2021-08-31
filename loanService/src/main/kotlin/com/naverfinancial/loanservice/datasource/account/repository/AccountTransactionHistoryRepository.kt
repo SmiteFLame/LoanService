e53b1297b3ff528b4592143588b8c603c027fd00
SmiteFLame <lukigreen@naver.com>
2021-08-31 15:38:16 +0900

    FIX: Page리스트 변경

--- loanService/src/main/kotlin/com/naverfinancial/loanservice/datasource/account/repository/AccountTransactionHistoryRepository.kt
+++ loanService/src/main/kotlin/com/naverfinancial/loanservice/datasource/account/repository/AccountTransactionHistoryRepository.kt
@@ -1,9 +1,10 @@
 package com.naverfinancial.loanservice.datasource.account.repository
 
 import com.naverfinancial.loanservice.datasource.account.dto.AccountTransactionHistory
+import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface AccountTransactionHistoryRepository : JpaRepository<AccountTransactionHistory, Integer> {
-    fun findAccountTransactionHistoriesByAccountId(accountId: Int, pageable: Pageable): List<AccountTransactionHistory>
+    fun findAccountTransactionHistoriesByAccountId(accountId: Int, pageable: Pageable): Page<AccountTransactionHistory>
 }
