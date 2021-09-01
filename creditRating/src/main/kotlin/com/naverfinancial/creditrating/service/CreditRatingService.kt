cf40df2216d1537fab938244e83ded45a4743269
SmiteFLame <lukigreen@naver.com>
2021-09-01 11:11:05 +0900

    FEAT: Interface 주석 추가

--- creditRating/src/main/kotlin/com/naverfinancial/creditrating/service/CreditRatingService.kt
+++ creditRating/src/main/kotlin/com/naverfinancial/creditrating/service/CreditRatingService.kt
@@ -4,6 +4,9 @@ import com.naverfinancial.creditrating.datasource.creditRatingSearch.dto.CreditR
 import com.naverfinancial.creditrating.datasource.user.dto.User
 
 interface CreditRatingService {
+    // CB 서버에서 등급을 받아오기
     fun selectGrade(user: User): CreditRatingSearchResult
+
+    // 대출 가능 여부를 판단하기
     fun evaluateLoanAvailability(grade: Int): Boolean
 }
