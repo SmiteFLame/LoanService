723906c13155d552ec2b233fbc62375a5d205b90
SmiteFLame <lukigreen@naver.com>
2021-09-08 17:55:53 +0900

    FIX: Cache Application
    만약 Spring Cache를 사용한다면

--- loanService/src/main/kotlin/com/naverfinancial/loanservice/LoanServiceApplication.kt
+++ loanService/src/main/kotlin/com/naverfinancial/loanservice/LoanServiceApplication.kt
@@ -2,7 +2,9 @@ package com.naverfinancial.loanservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 
+@EnableCaching
 @SpringBootApplication
 class LoanServiceApplication
 
