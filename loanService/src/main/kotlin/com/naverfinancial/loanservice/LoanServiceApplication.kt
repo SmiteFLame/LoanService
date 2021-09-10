18217fa73de4c41623accc057fbf6b2ae3026df3
SmiteFLame <lukigreen@naver.com>
2021-09-10 18:46:52 +0900

    FIX: 필요없는 @EnableCaching 제거

--- loanService/src/main/kotlin/com/naverfinancial/loanservice/LoanServiceApplication.kt
+++ loanService/src/main/kotlin/com/naverfinancial/loanservice/LoanServiceApplication.kt
@@ -4,7 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cache.annotation.EnableCaching
 
-@EnableCaching
 @SpringBootApplication
 class LoanServiceApplication
 
