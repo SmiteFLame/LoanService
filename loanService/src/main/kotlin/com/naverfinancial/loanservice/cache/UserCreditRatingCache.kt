a9b31f64890ad1938fefeb0eca45d5253a8eff41
SmiteFLame <lukigreen@naver.com>
2021-09-14 17:03:38 +0900

    ADD: Cache 타임아웃 기능 추가

--- loanService/src/main/kotlin/com/naverfinancial/loanservice/cache/UserCreditRatingCache.kt
+++ loanService/src/main/kotlin/com/naverfinancial/loanservice/cache/UserCreditRatingCache.kt
@@ -2,9 +2,13 @@ package com.naverfinancial.loanservice.cache
 
 import com.naverfinancial.loanservice.datasource.user.dto.UserCreditRating
 import com.naverfinancial.loanservice.wrapper.LRUCache
+import java.util.*
+import kotlin.collections.HashMap
+import kotlin.concurrent.timer
 
 object UserCreditRatingCache {
     private val cache = LRUCache<String, UserCreditRating>(3)
+    private val cacheVersion = HashMap<String, UUID>()
 
     fun getCache(ndi: String): UserCreditRating? {
         return cache[ndi]
@@ -14,7 +18,17 @@ object UserCreditRatingCache {
         if (cache[ndi] != null) {
             cache.remove(ndi)
         }
+        cacheVersion[ndi] = UUID.randomUUID()
         cache[ndi] = userCreditRating
+
+        Thread {
+            val uuid = cacheVersion[ndi]
+            Thread.sleep(10000)
+            if(cacheVersion[ndi] == uuid){
+                cache.remove(ndi)
+                cacheVersion.remove(ndi)
+            }
+        }.start()
     }
 
     fun removeCache(ndi: String) {
