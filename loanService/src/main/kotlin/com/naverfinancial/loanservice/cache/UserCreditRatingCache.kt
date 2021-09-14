6ef25647c85f85bef7d2b66c4e9c0402e8f00697
SmiteFLame <lukigreen@naver.com>
2021-09-14 10:37:52 +0900

    ADD: 캐시 추가

--- loanService/src/main/kotlin/com/naverfinancial/loanservice/cache/UserCreditRatingCache.kt
+++ loanService/src/main/kotlin/com/naverfinancial/loanservice/cache/UserCreditRatingCache.kt
@@ -1,18 +1,25 @@
 package com.naverfinancial.loanservice.cache
 
 import com.naverfinancial.loanservice.datasource.user.dto.UserCreditRating
+import com.naverfinancial.loanservice.wrapper.LRUCache
+import java.util.*
+import kotlin.collections.LinkedHashMap
 
 object UserCreditRatingCache {
-    private var cache = HashMap<String, UserCreditRating>()
+     private val cache = LRUCache<String, UserCreditRating>(3)
 
-    /**
-     * 캐시 구현을 해야되는 기능
-     * 1. 캐시 추가
-     * 2. 캐시 타임 아웃 삭제
-     * 3. 데이터 변경시 삭제
-     *
-     * 수정 완료후
-     * Cache 위치 수정(Service가 아니라 Controller에 붙이기)
-     *
-     */
+     fun getCache(ndi: String) : UserCreditRating?{
+          return cache[ndi]
+     }
+
+     fun insertCache(ndi : String, userCreditRating: UserCreditRating){
+          if(cache[ndi] != null){
+               cache.remove(ndi)
+          }
+          cache[ndi] = userCreditRating
+     }
+
+     fun removeCache(ndi : String){
+          cache.remove(ndi)
+     }
 }
