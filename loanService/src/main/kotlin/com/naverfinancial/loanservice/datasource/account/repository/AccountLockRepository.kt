d3bcf25d32f5e26036c6381d11f03715fc82b7e3
SmiteFLame <lukigreen@naver.com>
2021-09-15 11:40:39 +0900

    FIX: Lock Timeout 추가

--- loanService/src/main/kotlin/com/naverfinancial/loanservice/datasource/account/repository/AccountLockRepository.kt
+++ loanService/src/main/kotlin/com/naverfinancial/loanservice/datasource/account/repository/AccountLockRepository.kt
@@ -3,11 +3,14 @@ package com.naverfinancial.loanservice.datasource.account.repository
 import com.naverfinancial.loanservice.datasource.account.dto.Account
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Lock
+import org.springframework.data.jpa.repository.QueryHints
 import org.springframework.stereotype.Repository
 import javax.persistence.LockModeType
+import javax.persistence.QueryHint
 
 @Repository
 interface AccountLockRepository : JpaRepository<Account, Int> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @QueryHints(value = [QueryHint(name = "javax.persistence.lock.timeout", value = "10000")])
     fun findAccountByAccountId(accountId: Int): Account?
 }
