3ee9c5d3692995da7201c331bc9c91af16c15271
SmiteFLame <lukigreen@naver.com>
2021-09-16 14:25:21 +0900

    FIX: Cache, Cache Time 따로 분리

--- loanService/src/main/kotlin/com/naverfinancial/loanservice/cache/UserCreditRatingCache.kt
+++ loanService/src/main/kotlin/com/naverfinancial/loanservice/cache/UserCreditRatingCache.kt
@@ -12,9 +12,10 @@ object UserCreditRatingCache {
     fun getCache(ndi: String): UserCreditRating? {
         if (cache[ndi] == null) {
             return null
-        } else if (cache[ndi]!!.createdDate.time + cacheTime <= Timestamp(System.currentTimeMillis()).time) {
+        } else if (cache[ndi]!!.cacheTimestamp.time + cacheTime <= Timestamp(System.currentTimeMillis()).time) {
             return null
         }
+        updateCache(ndi)
         return cache[ndi]
     }
 
@@ -24,6 +25,12 @@ object UserCreditRatingCache {
             cache.remove(ndi)
         }
         cache[ndi] = userCreditRating
+        updateCache(ndi)
+    }
+
+    // Cache 시간 변경
+    private fun updateCache(ndi : String){
+        cache[ndi]!!.cacheTimestamp = Timestamp(System.currentTimeMillis())
     }
 
     // Cache 삭제
